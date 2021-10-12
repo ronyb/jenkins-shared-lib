d91d21eea31ca8b9879e6d2963c9843844f3b28d
Rony Byalsky <rony.byalsky@gmail.com>
2021-10-12 18:09:55 +0300

    Update Pipeline.groovy

--- src/com/ronyb/Pipeline.groovy
+++ src/com/ronyb/Pipeline.groovy
@@ -2,6 +2,16 @@ package com.ronyb
 
 public class Pipeline {
   
+  private String creatorName;
+  
+  public Pipeline(String creatorName) {
+    this.creatorName = creatorName;
+  }
+
+  public void helloWorld() {
+    echo "Hello world! Creator name: ${creatorName}"
+  }
+  
   public int add(int a, int b) {
     return a + b;
   }
